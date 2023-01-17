80c7e75f85e338be78d335e895e05be2794d800b
cacau <anaclaudia.sza@outlook.com>
2023-01-17 16:40:09 -0300

    feat: add scrape_next_page_link func

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -28,7 +28,12 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_page_ref = '/'
+    while next_page_ref:
+        return selector.css('a.next.page-numbers::attr(href)').get()
+    else:
+        None
 
 
 # Requisito 4
