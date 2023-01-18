c9c556b43c90d0d22d3cebfd8ef4fb90f39fd5c5
cacau <anaclaudia.sza@outlook.com>
2023-01-18 18:35:29 -0300

    fix: refactor search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -14,13 +14,9 @@ def return_tuple_list(news: str) -> List[Dict]:
 
 # Requisito 6
 def search_by_title(title: str) -> List[Dict]:
-    tuple_list = []
     news = search_news({'title': {'$regex': title, '$options': 'si'}})
 
-    for new in news:
-        tuple_list.append((new['title'], new['url']))
-
-    return tuple_list
+    return return_tuple_list(news)
 
 
 # Requisito 7
