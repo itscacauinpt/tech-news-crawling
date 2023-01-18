057d45e021e99207966b73f86890acd4eaf5f2b8
cacau <anaclaudia.sza@outlook.com>
2023-01-18 18:33:01 -0300

    feat: search by date + return_tuple function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,15 @@
 from tech_news.database import search_news
+
 from typing import List, Dict
+from datetime import datetime
+
+
+def return_tuple_list(news: str) -> List[Dict]:
+    tuple_list = []
+    for new in news:
+        tuple_list.append((new['title'], new['url']))
+
+    return tuple_list
 
 
 # Requisito 6
@@ -14,8 +24,14 @@ def search_by_title(title: str) -> List[Dict]:
 
 
 # Requisito 7
-def search_by_date(date):
-    """Seu código deve vir aqui"""
+def search_by_date(date: str) -> List[Dict]:
+    try:
+        date_formated = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+        news = search_news({'timestamp': {'$eq': date_formated}})
+
+        return return_tuple_list(news)
+    except ValueError:
+        raise ValueError('Data inválida')
 
 
 # Requisito 8
