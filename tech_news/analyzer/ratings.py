cd24c14cfc45f2775767493a3273addd277822f8
cacau <anaclaudia.sza@outlook.com>
2023-01-18 19:31:00 -0300

    feat: add top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,21 @@
+from tech_news.database import find_news
+
+from typing import List, Dict
+from operator import itemgetter
+
+
 # Requisito 10
-def top_5_news():
-    """Seu código deve vir aqui"""
+def top_5_news() -> List[Dict]:
+    news = find_news()
+
+    news_sorted = sorted(
+        news, key=itemgetter('comments_count', 'title'), reverse=True)
+
+    tuple_list = []
+    for new in news_sorted:
+        tuple_list.append((new['title'], new['url']))
+
+    return tuple_list[:5]
 
 
 # Requisito 11
