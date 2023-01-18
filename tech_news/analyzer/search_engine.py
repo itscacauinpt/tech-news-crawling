c31c824a28614703d337643a3db50bbe9438a39b
cacau <anaclaudia.sza@outlook.com>
2023-01-18 16:41:54 -0300

    feat: search by title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+from typing import List, Dict
+
+
 # Requisito 6
-def search_by_title(title):
-    """Seu código deve vir aqui"""
+def search_by_title(title: str) -> List[Dict]:
+    tuple_list = []
+    news = search_news({'title': {'$regex': title, '$options': 'si'}})
+
+    for new in news:
+        tuple_list.append((new['title'], new['url']))
+
+    return tuple_list
 
 
 # Requisito 7
