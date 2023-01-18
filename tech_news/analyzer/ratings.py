7ba4e6bb7a538d8c14b318cea41ea8c64308c923
cacau <anaclaudia.sza@outlook.com>
2023-01-18 20:24:16 -0300

    feat: add five_most_common func

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -2,6 +2,7 @@ from tech_news.database import find_news
 
 from typing import List, Dict
 from operator import itemgetter
+from collections import Counter
 
 
 # Requisito 10
@@ -9,7 +10,8 @@ def top_5_news() -> List[Dict]:
     news = find_news()
 
     news_sorted = sorted(
-        news, key=itemgetter('comments_count', 'title'), reverse=True)
+        news, key=itemgetter('comments_count', 'title'), reverse=True
+    )
 
     tuple_list = []
     for new in news_sorted:
@@ -19,5 +21,17 @@ def top_5_news() -> List[Dict]:
 
 
 # Requisito 11
-def top_5_categories():
-    """Seu código deve vir aqui"""
+def top_5_categories() -> List[Dict]:
+    news = find_news()
+
+    categories = []
+    for new in news:
+        categories.append(new['category'])
+
+    category_amount = Counter(sorted(categories)).most_common(5)
+
+    categories_most_common = []
+    for categoty_tuple in category_amount:
+        categories_most_common.append(categoty_tuple[0])
+
+    return categories_most_common
